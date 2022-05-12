bd3e3ce38d6d930aa5bd09f97356d9c770744020
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-12 16:10:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6285859047172'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6285859047172', 'Rull', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'Rull Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
