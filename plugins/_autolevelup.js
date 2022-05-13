41253b57da172528e3304383070d9feb5301bbcd
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-13 16:47:57 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,7 +11,7 @@ export function before(m) {
         m.reply(`
 Selamat, anda telah naik level!
 *${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
+gunakan Selamat🥳*
 	`.trim())
     }
 }
