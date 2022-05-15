e645fdc81db84783e5afe0d2ba763c0bad20c5ee
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-16 06:44:16 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,8 @@
 let handler =  m => m.reply(`
-╭─「 Donasi 」
-│ Donasi Seikhlasnya Bg
+╭─「 𝗗𝗢𝗡𝗔𝗦𝗜 」
+│ 𝖬𝖺𝗄𝖺𝗌𝗂𝗁 𝖸𝖺𝗇𝗀 𝖴𝖽𝖺𝗁 𝖣𝗈𝗇𝖺𝗌𝗂
 │ • https://saweria.co/Itsrullbot
+│ 𝘋𝘰𝘯𝘢𝘴𝘪 𝘚𝘦𝘪𝘩𝘬𝘭𝘢𝘴 𝘕𝘺𝘢 𝘈𝘫𝘢 𝘠𝘢 𝘒𝘢𝘬
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
