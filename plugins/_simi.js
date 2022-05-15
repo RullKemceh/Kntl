8180213e7bf58a605b3dae0575db5511775e06be
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-15 15:49:10 +0700

    Add files via upload

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -1,25 +1,23 @@
-// simi
-import fetch from 'node-fetch'
+//Fix simi
+//BOTCAHX
 
-export async function before(m) {
+let fetch = require('node-fetch')
+
+let handler = m => m
+
+handler.before = async (m) => {
     let chat = db.data.chats[m.chat]
-    if (chat.simi) {
+    if (chat.simi && !chat.isBanned) {
+        if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let url = `https://api-sv2.simsimi.net/v2/?text=${m.text}&lc=id&cf=true`
-        let res = await fetch(url)
+        let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'id' }))
+        if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        let {
-            response,
-            result
-        } = json.messages[0]
-        await this.chatRead(m.chat, m.sender, m.id || m.key.id)
-        await delay(1000)
-        await this.sendPresenceUpdate('composing', m.chat)
-        await delay(2000)
-        m.reply(response)
+        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('simi nya gk tau bang')
+        m.reply(json.success)
         return !0
     }
     return !0
 }
 
-const delay = time => new Promise(res => setTimeout(res, time))
+module.exports = handler
