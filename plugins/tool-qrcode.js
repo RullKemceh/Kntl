32ac5347b1175f0749e1d6b828b4240862ec681f
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-18 14:14:37 +0700

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,7 +1,7 @@
 
 import { toDataURL } from 'qrcode'
 
-let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+let handler = async (m, { conn, text }) => conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '(≧▽≦)', m)
 
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
