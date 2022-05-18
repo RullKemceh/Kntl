dee2433a06d139e31944442bae85fa6805169695
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-18 13:36:00 +0700

    Update blowjob.js

--- plugins/blowjob.js
+++ plugins/blowjob.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
    conn.sendButtonImg(m.chat, json, 'Wangy Wangy Wangy', watermark, 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['blowjob']
-handler.tags = ['hentai']
+handler.tags = ['internet']
 
 handler.command = /^(blowjob)$/i
 
