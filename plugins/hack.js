22d6b13e0c3b6b2f20c3f8555b0bfa1b3bbfc6f3
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-19 13:28:44 +0700

    Add files via upload

---
+++ plugins/hack.js
@@ -0,0 +1,26 @@
+const rewards = {
+  exp: 99999999999999999999999999999,
+  money: 999999999999999999999999999,
+  potion: 5,
+}
+const cooldown = 1
+let handler = async (m) => {
+  let user = global.db.data.users[m.sender]
+  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
+  let text = ''
+  for (let reward of Object.keys(rewards)) {
+    if (!(reward in user)) continue
+    user[reward] += rewards[reward]
+    text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
+  }
+  m.reply(text.trim())
+  user.lastclaim = new Date * 1
+}
+handler.help = ['hack', 'hackuang']
+handler.tags = ['xp']
+handler.command = /^(hack|hackuang)$/i
+handler.owner = true
+
+handler.cooldown = cooldown
+
+export default handler
