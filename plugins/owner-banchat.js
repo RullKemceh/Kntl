f66b320a2d80266c5b6b8fc23b0114593405f7e4
RullRaraa <105038032+RullRaraa@users.noreply.github.com>
2022-05-20 11:19:11 +0700

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('Oke Bang!')
+    m.reply('𝐷𝑂𝑁𝐸!')
     // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
